7a78dcaacfd94f1914521f70147a938525c99bab
CloudsInTheSky <claudionmoya@gmail.com>
2021-03-14 13:16:22 -0500

    created objects for each book in the list, added more variables for object deconstructing for const book

--- src/index.js
+++ src/index.js
@@ -3,39 +3,51 @@ import ReactDom from 'react-dom';
 
 import './main.css';
 
+const firstBook = {
+  img: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-03/2/14/enhanced/webdr11/enhanced-22820-1425322972-8.png',
+  title: 'To Kill a Mocking Bird',
+  author: 'Harper Lee'
+}
+const secondBook = {
+  img: 'https://storage.googleapis.com/du-prd/books/images/9780593318171.jpg',
+  title: 'Klara and the Sun',
+  author: 'Kazuo Ishiguro'
+}
+
 function Booklist() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          Content
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+
     </section>
   );
 }
 
-const Book = () => {
+
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <Image></Image>
-      <Title />
-      <Author />
+      <img src={props.img} alt="" width="150px" />
+      <h1>{props.title}</h1>
+      <h2>{props.author}</h2>
+      {props.children}
     </article>
   );
 };
-
-const Image = () => (<img
-  src="https://img.buzzfeed.com/buzzfeed-static/static/2015-03/2/14/enhanced/webdr11/enhanced-22820-1425322972-8.png"
-  alt="" width="150px"
-/>
-);
-
-const Title = () => <h1>To Kill a Mocking Bird</h1>;
-const Author = () => <h2>Harper Lee</h2>;
 
 // const Greet = () => {
 //   return React.createElement('div',{},
