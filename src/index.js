3bbfecaf130aaced6bb5694e27dfb3e8bd774ce5
CloudsInTheSky <claudionmoya@gmail.com>
2021-03-15 19:19:12 -0500

    displayed individual book lists, added books prop and data to their own js files

--- src/index.js
+++ src/index.js
@@ -3,57 +3,18 @@ import ReactDom from 'react-dom';
 
 import './main.css';
 
-const firstBook = {
-  img: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-03/2/14/enhanced/webdr11/enhanced-22820-1425322972-8.png',
-  title: 'To Kill a Mocking Bird',
-  author: 'Harper Lee'
-}
-const secondBook = {
-  img: 'https://storage.googleapis.com/du-prd/books/images/9780593318171.jpg',
-  title: 'Klara and the Sun',
-  author: 'Kazuo Ishiguro'
-}
+import { books } from './books';
+import Book from './book'
 
 function Booklist() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Content
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-
+      {books.map((book) => {
+        return <Book key={book.id} book={book}></Book>;
+      })}
     </section>
   );
 }
-
-
-const Book = (props) => {
-  console.log(props);
-  return (
-    <article className="book">
-      <img src={props.img} alt="" width="150px" />
-      <h1>{props.title}</h1>
-      <h2>{props.author}</h2>
-      {props.children}
-    </article>
-  );
-};
-
-// const Greet = () => {
-//   return React.createElement('div',{},
-//   React.createElement('h1', {}, 'hello world')
-//   );
-// };
 
 ReactDom.render(<Booklist />, document.getElementById('root')
 );
