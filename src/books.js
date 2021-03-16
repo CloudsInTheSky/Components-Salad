b828505017a4d5c5fa00f2353085b92f7103cb82
CloudsInTheSky <claudionmoya@gmail.com>
2021-03-15 19:20:34 -0500

    book data objects

---
+++ src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+    {
+        id: 1,
+        img: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-03/2/14/enhanced/webdr11/enhanced-22820-1425322972-8.png',
+        title: 'To Kill a Mocking Bird',
+        author: 'Harper Lee'
+    },
+    {
+        id: 2,
+        img: 'https://storage.googleapis.com/du-prd/books/images/9780593318171.jpg',
+        title: 'Klara and the Sun',
+        author: 'Kazuo Ishiguro'
+    },
+    {
+        id: 3,
+        img: 'https://i.pinimg.com/originals/e1/80/93/e180935c144c0ad62485c39d58691f31.jpg',
+        title: 'Taxi Driver',
+        author: 'Robert De Niro'
+    },
+];
